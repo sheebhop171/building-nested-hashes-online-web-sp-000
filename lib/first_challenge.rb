a699e2256e472ae42442bf6b24a8ff8ca56c31bf
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-01 20:34:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,16 @@
 def first_challenge
    epic_tragedy = {
-     montague: {
-       patriarch: {}
-       matriarch: {}
-       hero: {}
-       hero_friends: []
+    :montague => {
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => []
      }
      capulet: {
-       patriarch: {}
-       matriarch: {}
-       heroine: {}
-       heroine_friends: []
+       :patriarch => {}
+       :matriarch => {}
+       :heroine => {}
+       :heroine_friends => []
      }
    }
 end
